dd4fe8d29be2eadffd4947a9ce25607bf41011c8
jpaul <jpaul@agero.com>
2020-04-08 21:03:41 +0530

    Initial Update

---
+++ SimpleWebApi/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using SimpleWebApi.Models;
+using SimpleWebApi.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace SimpleWebApi.Controllers
+{
+    public class HomeController:ApiController
+    {
+        
+        public WebApiResponse GetDetails(string id)
+        {
+            return SimpleWebApiService.GetResponse(id);
+        }
+       
+    }
+}
---
+++ SimpleWebApi/Service/SimpleWebApiService.cs
@@ -0,0 +1,37 @@
+﻿using SimpleWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+
+namespace SimpleWebApi.Service
+{
+    public class SimpleWebApiService
+    {
+        public static WebApiResponse GetResponse(string id)
+        {
+            using (var client = new System.Net.Http.HttpClient())
+            {
+                client.BaseAddress = new Uri("https://jsonplaceholder.typicode.com/todos/");
+                //HTTP GET
+                var responseTask = client.GetAsync(id);
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+
+                    var readTask = result.Content.ReadAsAsync<WebApiResponse>();
+                    readTask.Wait();
+
+                   return readTask.Result;
+                }
+                else
+                {
+                    return new WebApiResponse();
+                }
+            }
+        }
+    }
+}
