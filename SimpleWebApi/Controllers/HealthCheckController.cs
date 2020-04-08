bcb45bf09013d12e3fec991ac3d4cab624a32921
jpaul <jpaul@agero.com>
2020-04-08 21:07:55 +0530

    Final Migrated Code

---
+++ SimpleWebApi/Controllers/HealthCheckController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace SimpleWebApi.Controllers
+{
+    public class HealthCheckController:ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult HealthCheck()
+        {
+            return Ok<string>("Endpoint is working");
+        }
+    }
+}
